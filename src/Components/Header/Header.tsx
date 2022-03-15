9ea788241f2fbb8582dbf52a2c30daee03f54bf7
unknown <aqibnali@gmail.com>
2022-03-15 01:04:36 -0700

    first commit

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+import "./Header.scss";
+
+const Header = () => {
+    return (
+        <>
+            {/*----------------------Top Section --------------------*/}
+            <div className={''}>
+                <Container>
+                    <Row>
+                        <Col>
+
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </>
+    );
+};
+
+export default Header;
